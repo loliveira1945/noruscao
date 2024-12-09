f80ba1c438b9f3eb9c2b293c636967880dcc177d
loliveira1945 <lucasalves1945@icloud.com>
2024-12-09 17:20:10 -0300

    test realtime

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://webwidgets.365scores.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   transpileDependencies: true,
   configureWebpack: {},
 });
