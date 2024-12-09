78b4666ef5876fb12eafdc23898bb0d66ab838db
loliveira1945 <lucasalves1945@icloud.com>
2024-12-09 01:02:12 -0300

    vue config

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.esm-bundler.js',
-      },
-    },
-  },
-  define: {
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // Define explicitamente a flag
-  },
+  configureWebpack: {},
 });
