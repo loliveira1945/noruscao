dad5e78629459147836d26de7e856a643ea6ca1e
loliveira1945 <lucasalves1945@icloud.com>
2024-12-09 20:00:59 -0300

    features 3 - mobile

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        window.scrollTo(0, 1);
+    }, 0);
+});
