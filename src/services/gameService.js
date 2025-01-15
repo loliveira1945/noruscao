06654ba17800f7cfcf5578af6b96feb0ef77d930
Lucas Oliveira <lucasalves1945@icloud.com>
2025-01-15 18:44:40 -0300

    test

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,8 +1,8 @@
 import { processGame } from './proccesGame';
 
 export async function fetchGame() {
-  // const API_URL = "https://webwidgets.365scores.com/web/games/current/?appTypeId=8&langId=31&timezoneName=America/Sao_Paulo&competitors=1270&includeTopBettingOpportunity=1";
-  const API_URL = "https://webws.365scores.com/web/game/?appTypeId=5&langId=31&timezoneName=America/Sao_Paulo&userCountryId=21&gameId=4306668&matchupId=1270-19570-114&topBookmaker=161"
+  const API_URL = "https://webwidgets.365scores.com/web/games/current/?appTypeId=8&langId=31&timezoneName=America/Sao_Paulo&competitors=1270&includeTopBettingOpportunity=1";
+  
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
